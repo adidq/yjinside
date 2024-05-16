91a05dcab45661826f3d25d40f0c37bf4b23635b
adidq <145259647+adidq@users.noreply.github.com>
2024-05-16 07:33:45 +0000

    update

--- function/forms.py
+++ function/forms.py
@@ -5,3 +5,9 @@ from wtforms.validators import DataRequired, Length, EqualTo, NumberRange, Email
 class loginForm(FlaskForm):
     user_id = StringField('user_id', validators=[DataRequired('Required')])
     password = PasswordField('password', validators=[DataRequired('Required')])
+
+class registerForm(FlaskForm):
+    user_id = StringField('user_id', validators=[DataRequired('Required'), Length(min=3, max=16)])
+    user_name = StringField('user_name', validators=[DataRequired('Required'), Length(min=3, max=16)])
+    user_email = EmailField('email', validators=[DataRequired('Required'), Email()])
+    password = PasswordField('password', validators=[DataRequired('Required'), Length(min=8)])
--- routes/auth.py
+++ routes/auth.py
@@ -5,6 +5,7 @@ import hashlib
 
 _auth = Blueprint('auth', __name__, url_prefix="/")
 
+#로그인
 @_auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = loginForm()
@@ -33,6 +34,16 @@ def login():
             return render_template('login.html', form=form)
     return render_template('login.html', form=form)
 
+#회원가입
+@_auth.route('/register')
+def register():
+    #로그인된 유저가 접속시도시 홈으로
+    if 'username' in session:
+        return redirect('/')
+    #가입시 이메일인증
+    return "개발중"
+
+#로그아웃
 @_auth.route('/logout')
 def logout():
     if session:
@@ -40,4 +51,3 @@ def logout():
         return redirect('/')
     else:
         return redirect('/')
-    
